82e52bd69ff40298ee8ddc741cb469d2f07c1513
LSM <jake3401@naver.com>
2025-05-08 10:41:37 +0900

    2025-05-08 웹 개발 프로젝트 기본 설정(이승민)

---
+++ src/main/java/com/plantopia/PlantopiaApplication.java
@@ -0,0 +1,13 @@
+package com.plantopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantopia/PlantopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
